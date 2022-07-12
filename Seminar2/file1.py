dc74c29c6c964982c4fd3ab7bf9c09c23d6f9072
Andrey <finya2012@mail.ru>
2022-07-12 21:03:57 +0400

    Учтено введение отрицательных значений

--- Seminar2/file1.py
+++ Seminar2/file1.py
@@ -1,5 +1,8 @@
 number = float(input('Введите вещественное число: '))
 
+if number < 0:
+    number = number * -1
+
 strNunber = str(number)
 strNunber = strNunber.replace('.','')
 lstStr = list(strNunber)
